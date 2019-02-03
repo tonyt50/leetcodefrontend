29a1bceae241e554cd1d214a3bf2cbd2b649ded6
Antony Thompson <tonyt@Antonys-MacBook-Pro.local>
2019-02-03 19:28:19 +0000

    connect for basic frame work

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,88 @@
-import React, { Component, ChangeEvent } from "react";
+import React, { Component } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
-import getLongestSubstringLength from "./longestsubstring";
 
-class App extends Component<{}, { input: string; result: string }> {
-  state = {
-    input: "Hello World",
-    result: ""
+type SquareValues = "X" | "O" | "";
+
+type Squares = SquareValues[][];
+
+interface AppState {
+  squares: Squares;
+  currentPlayer: SquareValues;
+}
+
+class App extends Component<{}, AppState> {
+  state: AppState = {
+    squares: createInitialSquares(),
+    currentPlayer: "X"
   };
 
-  onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    this.setState({ input: value });
+  onConnect4Click = (x: number, y: number) => {
+    console.log(`Row: ${x}, Col: ${y}`);
+
+    this.setState(prevState => {
+      const { squares, currentPlayer } = prevState;
+
+      if (squares[y][x]) {
+        return { ...prevState };
+      }
+
+      const newSquares: Squares = [...squares];
+      newSquares[y] = [...newSquares[y]];
+      newSquares[y][x] = currentPlayer;
+
+      return {
+        squares: newSquares,
+        currentPlayer: currentPlayer === "X" ? "O" : "X"
+      };
+    });
   };
 
-  onButtonClick = (bunttonFunction: "longestSubstring" | "toUpperCase") => {
-    const input = this.state.input;
-    let result = "";
-    if (bunttonFunction === "longestSubstring") {
-      result = getLongestSubstringLength(input).toString();
-    }
-    if (bunttonFunction === "toUpperCase") {
-      result = input.toUpperCase();
-    }
-    this.setState({ result });
+  newGame = () => {
+    this.setState({
+      currentPlayer: "X",
+      squares: createInitialSquares()
+    });
   };
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p className="Result">{this.state.input}</p>
-          <input value={this.state.input} onChange={this.onChangeInput} />
-          <button onClick={() => this.onButtonClick("toUpperCase")}>
-            Set upper case
-          </button>
-          <button onClick={() => this.onButtonClick("longestSubstring")}>
-            Longest sub string
-          </button>
-          <p className="Result">{this.state.result}</p>
-        </header>
+        <body className="App-body">
+          <p>Play connect 4</p>
+          <button onClick={this.newGame}>New Game</button>
+          <div>
+            {this.state.squares.map((row, y) => (
+              <div className="Connect4Row" key={y}>
+                {row.map((squareValue, x) => (
+                  <button
+                    className="Connect4Button"
+                    onClick={() => this.onConnect4Click(x, y)}
+                    key={`${x}-${y}`}
+                  >
+                    {squareValue}
+                  </button>
+                ))}
+              </div>
+            ))}
+          </div>
+        </body>
       </div>
     );
   }
 }
 
 export default App;
+
+function createInitialSquares(): SquareValues[][] {
+  return [
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""]
+  ];
+}
