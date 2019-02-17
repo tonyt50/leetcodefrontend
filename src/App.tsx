57bb4bb7ae3ae0361078975140ecbaa19aa7e727
Antony Thompson <tonyt@Antonys-MacBook-Pro.local>
2019-02-17 20:06:29 +0000

    wow we got some cooll vectoring working thanks Jed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
+import { Vector } from "./types";
 
 type SquareValues = "X" | "O" | "";
 
@@ -9,48 +10,57 @@ type Squares = SquareValues[][];
 interface AppState {
   squares: Squares;
   currentPlayer: SquareValues;
+  winner: "X" | "O" | undefined;
 }
 
 class App extends Component<{}, AppState> {
-  state: AppState = {
-    squares: createInitialSquares(),
-    currentPlayer: "X"
-  };
+  state: AppState = createInitialAppState();
 
   onConnect4Click = (x: number, y: number) => {
     console.log(`Row: ${x}, Col: ${y}`);
+    const rowToPlaceSquare = findEmptyRowInColumn(this.state.squares, x);
+
+    if (this.state.winner) {
+      return;
+    }
+
+    if (rowToPlaceSquare === undefined) {
+      return;
+    }
 
     this.setState(prevState => {
       const { squares, currentPlayer } = prevState;
 
-      if (squares[y][x]) {
-        return { ...prevState };
-      }
-
       const newSquares: Squares = [...squares];
-      newSquares[y] = [...newSquares[y]];
-      newSquares[y][x] = currentPlayer;
+      newSquares[rowToPlaceSquare] = [...newSquares[rowToPlaceSquare]];
+      newSquares[rowToPlaceSquare][x] = currentPlayer;
+
+      const winner = calculateWinner(newSquares, { x, y: rowToPlaceSquare });
 
       return {
         squares: newSquares,
-        currentPlayer: currentPlayer === "X" ? "O" : "X"
+        currentPlayer: currentPlayer === "X" ? "O" : "X",
+        winner
       };
     });
   };
 
   newGame = () => {
-    this.setState({
-      currentPlayer: "X",
-      squares: createInitialSquares()
-    });
+    this.setState(createInitialAppState());
   };
 
   render() {
+    const { winner, currentPlayer } = this.state;
     return (
       <div className="App">
-        <body className="App-body">
+        <div className="App-body">
           <p>Play connect 4</p>
-          <button onClick={this.newGame}>New Game</button>
+          <button onClick={this.newGame} className="GameButton">
+            New Game
+          </button>
+          <p>
+            {winner ? `${winner} is the winner!` : `${currentPlayer}'s turn!`}
+          </p>
           <div>
             {this.state.squares.map((row, y) => (
               <div className="Connect4Row" key={y}>
@@ -66,13 +76,21 @@ class App extends Component<{}, AppState> {
               </div>
             ))}
           </div>
-        </body>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+function createInitialAppState(): AppState {
+  return {
+    squares: createInitialSquares(),
+    currentPlayer: "X",
+    winner: undefined
+  };
+}
 
 function createInitialSquares(): SquareValues[][] {
   return [
@@ -85,4 +103,124 @@ function createInitialSquares(): SquareValues[][] {
     ["", "", "", "", "", "", "", ""],
     ["", "", "", "", "", "", "", ""]
   ];
+}
+
+function findEmptyRowInColumn(
+  squares: SquareValues[][],
+  x: number
+): number | undefined {
+  for (const [index, row] of squares.entries()) {
+    if (row[x] !== "") {
+      return index === 0 ? undefined : index - 1;
+    }
+  }
+
+  return squares.length - 1;
+}
+
+type directions = "horizontal" | "vertical" | "diagonalLeft" | "diagonalRight";
+
+function calculateWinner(
+  squares: SquareValues[][],
+  lastChange: Vector
+): "X" | "O" | undefined {
+  const results: { [key in directions]: number } = {
+    diagonalLeft: 1,
+    diagonalRight: 1,
+    horizontal: 1,
+    vertical: 1
+  };
+
+  const team = squares[lastChange.y][lastChange.x];
+
+  for (const direction in results) {
+    switch (direction) {
+      case "horizontal":
+        results.horizontal = calculateNoOfContinousTeamAlongVector(
+          squares,
+          lastChange,
+          { x: 1, y: 0 }
+        );
+
+      case "vertical":
+        results.vertical = calculateNoOfContinousTeamAlongVector(
+          squares,
+          lastChange,
+          { x: 0, y: 1 }
+        );
+
+      case "diagonalLeft":
+        results.diagonalLeft = calculateNoOfContinousTeamAlongVector(
+          squares,
+          lastChange,
+          { x: -1, y: 1 }
+        );
+
+      case "diagonalRight":
+        results.diagonalRight = calculateNoOfContinousTeamAlongVector(
+          squares,
+          lastChange,
+          { x: 1, y: 1 }
+        );
+    }
+  }
+
+  if (Object.values(results).some(value => value >= 4)) {
+    return team === "" ? undefined : team;
+  }
+
+  return undefined;
+}
+
+function calculateNoOfContinousTeamAlongVector(
+  squares: SquareValues[][],
+  startPosition: Vector,
+  vector: Vector
+): number {
+  let result = 1;
+  const team = getSquareValue(squares, startPosition);
+
+  let newVector = transformVector(startPosition, vector);
+  while (
+    isVectorInsideGrid(squares, newVector) &&
+    getSquareValue(squares, newVector) === team
+  ) {
+    result++;
+    newVector = transformVector(newVector, vector);
+  }
+
+  newVector = transformVector(startPosition, inverseVector(vector));
+  while (
+    isVectorInsideGrid(squares, newVector) &&
+    getSquareValue(squares, newVector) === team
+  ) {
+    result++;
+    newVector = transformVector(newVector, inverseVector(vector));
+  }
+  return result;
+}
+
+function isVectorInsideGrid(
+  squares: SquareValues[][],
+  { x, y }: Vector
+): boolean {
+  if (x < 0 || y < 0 || y > squares.length - 1 || x > squares[0].length - 1) {
+    return false;
+  }
+  return true;
+}
+
+function getSquareValue(
+  squares: SquareValues[][],
+  position: Vector
+): SquareValues {
+  return squares[position.y][position.x];
+}
+
+function transformVector(vectorA: Vector, vectorB: Vector): Vector {
+  return { x: vectorA.x + vectorB.x, y: vectorA.y + vectorB.y };
+}
+
+function inverseVector({ x, y }: Vector): Vector {
+  return { x: -x, y: -y };
 }
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,8 @@
 export interface Letters {
   [key: string]: number;
 }
+
+export interface Vector {
+  x: number;
+  y: number;
+}
