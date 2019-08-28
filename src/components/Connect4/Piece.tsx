0f63bece740a69446803677bc61c1929e435bc2f
Antony Thompson <tonyt@antonys-mbp.home>
2019-08-28 08:17:34 +0100

    before intro of styled components for pieces

--- src/components/Connect4/Piece.tsx
+++ src/components/Connect4/Piece.tsx
@@ -1,13 +1,32 @@
 import React, { FC } from "react";
-
+import { keyframes } from "styled-components";
 interface PieceProps {
   value: "X" | "O";
   color: "red" | "yellow";
+  className?: "" | "counter" | "pieceAnimDrop";
 }
+export const PieceDrop = keyframes`
+  from {transform: translateY(-70px)}
+  to {transform: translateY(0)}
+`;
 
-const Piece: FC<PieceProps> = ({ value, color }) => {
+export const PieceAnimDrop: FC = () => {
   return (
-    <svg height="30" width="30" fill={color} stroke-width="3">
+    <span className={"pieceAnimDrop"}>
+      animation: ${PieceDrop}; animation-duration: 0.5s;
+    </span>
+  );
+};
+
+const Piece: FC<PieceProps> = ({ value, color, className = "" }) => {
+  return (
+    <svg
+      className={className}
+      height="30"
+      width="30"
+      fill={color}
+      stroke-width="3"
+    >
       <circle
         cx="15"
         cy="15"
@@ -24,5 +43,17 @@ const Piece: FC<PieceProps> = ({ value, color }) => {
 
 export const XPiece: FC = () => <Piece value="X" color="red" />;
 export const OPiece: FC = () => <Piece value="O" color="yellow" />;
+export const XPieceAnimDrop: FC = () => (
+  <g>
+    <PieceAnimDrop />
+    <Piece value="X" color="red" className="pieceAnimDrop" />
+  </g>
+);
+export const OPieceAnimDrop: FC = () => (
+  <g>
+    <PieceAnimDrop />
+    <Piece value="O" color="yellow" className="pieceAnimDrop" />
+  </g>
+);
 
 export default Piece;
