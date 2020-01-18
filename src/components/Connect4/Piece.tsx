16e558d0524cc98113bb19274fd8229bf8106ea5
tonyt50 <tony_t50@hotmail.com>
2020-01-18 16:58:09 +0000

    got shodow working and svg circle

--- src/components/Connect4/Piece.tsx
+++ src/components/Connect4/Piece.tsx
@@ -24,19 +24,152 @@ const StyledSVGPiece = styled.svg<PieceProps>`
 export const ThisPiece: FC<PieceProps> = ({ value, color, drop }) => {
   console.log("Piece ", drop, color, value);
   return (
-    <StyledSVGPiece height="30" width="30" drop={drop || 0}>
-      <circle
-        cx="15"
-        cy="15"
-        r="12"
-        fill={color}
-        stroke="black"
-        strokeWidth="2"
-        alignmentBaseline="middle"
-      />
-      <text x="15" y="22" fill="black" textAnchor="middle" fontSize="75%">
-        {value}
-      </text>
+    <StyledSVGPiece height="40" width="40" drop={drop || 0}>
+      <defs id="defs2">
+        <filter id="filter4598" style={{ colorInterpolation: "sRGB" }}>
+          <feGaussianBlur
+            id="feGaussianBlur4572"
+            result="result0"
+            in="SourceAlpha"
+            stdDeviation="2.3"
+          />
+          <feMorphology
+            id="feMorphology4574"
+            result="result1"
+            radius="6.6"
+            in="SourceAlpha"
+          />
+          <feGaussianBlur
+            id="feGaussianBlur4576"
+            in="result1"
+            stdDeviation="8.9"
+          />
+          <feColorMatrix
+            id="feColorMatrix4578"
+            result="result91"
+            values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 0.3 0"
+          />
+          <feComposite
+            id="feComposite4580"
+            in2="result91"
+            result="result2"
+            operator="out"
+            in="result0"
+          />
+          <feGaussianBlur
+            id="feGaussianBlur4582"
+            result="result4"
+            stdDeviation="1.7"
+          />
+          <feDiffuseLighting id="feDiffuseLighting4586" surfaceScale="10">
+            <feDistantLight
+              id="feDistantLight4584"
+              elevation="45"
+              azimuth="225"
+            />
+          </feDiffuseLighting>
+          <feBlend id="feBlend4588" mode="multiply" in2="SourceGraphic" />
+          <feComposite
+            id="feComposite4590"
+            result="result3"
+            operator="in"
+            in2="SourceAlpha"
+          />
+          <feSpecularLighting
+            id="feSpecularLighting4594"
+            specularExponent="17.9"
+            surfaceScale="5"
+            in="result4"
+          >
+            <feDistantLight
+              id="feDistantLight4592"
+              elevation="45"
+              azimuth="225"
+            />
+          </feSpecularLighting>
+          <feComposite
+            result="fbSourceGraphic"
+            id="feComposite4596"
+            operator="atop"
+            in2="result3"
+          />
+          <feColorMatrix
+            id="feColorMatrix4600"
+            values="0 0 0 -1 0 0 0 0 -1 0 0 0 0 -1 0 0 0 0 1 0"
+            in="fbSourceGraphic"
+            result="fbSourceGraphicAlpha"
+          />
+          <feColorMatrix
+            in="fbSourceGraphic"
+            type="luminanceToAlpha"
+            result="result2"
+            id="feColorMatrix4602"
+          />
+          <feDiffuseLighting
+            surfaceScale="15"
+            result="result1"
+            diffuseConstant="0.5"
+            in="result2"
+            lighting-color="#ffffff"
+            id="feDiffuseLighting4604"
+          >
+            <feDistantLight
+              elevation="10"
+              azimuth="225"
+              id="feDistantLight4606"
+            />
+          </feDiffuseLighting>
+          <feComposite
+            k3="0.8"
+            k2="1"
+            operator="arithmetic"
+            result="result9"
+            id="feComposite4608"
+            in2="fbSourceGraphic"
+          />
+          <feComposite
+            in="result9"
+            operator="in"
+            result="result7"
+            id="feComposite4610"
+            in2="fbSourceGraphic"
+          />
+        </filter>
+        <filter id="shadow" x="-20%" y="-20%" width="200%" height="200%">
+          <feGaussianBlur stdDeviation="2 2" result="shadow" />
+          <feOffset dx="6" dy="6" />
+        </filter>
+      </defs>
+      <g id="layer1">
+        <circle
+          r="20"
+          cy="20"
+          cx="20"
+          style={{
+            fill: color,
+            fillOpacity: 1,
+            filter: "url(#filter4598)"
+          }}
+        />
+        <text
+          style={{ filter: "url(#shadow)", fill: "black" }}
+          textAnchor="middle"
+          font-size="xx-large"
+          x="17"
+          y="27"
+        >
+          {value}
+        </text>
+        <text
+          style={{ fill: color }}
+          textAnchor="middle"
+          font-size="xx-large"
+          x="20"
+          y="31"
+        >
+          {value}
+        </text>
+      </g>
     </StyledSVGPiece>
   );
 };
